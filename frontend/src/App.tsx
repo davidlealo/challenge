a9d6d870e981c01e0312b0af5bbddf3120dfeae0
davidlealo <davidlealo@gmail.com>
2024-06-13 21:53:13 -0400

    Fundacion handleSumit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,14 +7,19 @@ function App() {
     const file = event.target.files ?? []
    console.log(file)
   }
+
+  const handleSumit = (event: React.FormEvent<HTMLElement>) =>{
+    event.preventDefault()
+    console.log('TODO')
+  }
   return (
     <>
       <h4>App Challenge</h4>
-      <form>
+      <form onSubmit={handleSumit}>
         <label>
         <input onChange={handleInputChange} type="file" name="file" id="file" accept='.csv'/>
         </label>
-        
+
         <button>
           Subir archivo
         </button>
