546405744363ea3d40264b71568ab224bb21967a
davidlealo <davidlealo@gmail.com>
2024-06-14 18:16:41 -0400

    faltaba la q al buscar

--- frontend/src/services/search.ts
+++ frontend/src/services/search.ts
@@ -1,17 +1,15 @@
-import { type Data } from "./types"
-import { type apiSearchResponse } from "./types"
+import { type Data, type apiSearchResponse } from "./types"
 import { API_HOST } from "../config"
 
-export const searchData = async (search: string): Promise<[Error?, Data?]> =>{
+export const searchData = async (search: string): Promise<[Error?, Data?]> => {
+    try {
+        const res = await fetch(`http://${API_HOST}/api/users?q=${search}`) // Corrige el parámetro de consulta
 
-    try{
-        const res = await fetch(`http://${API_HOST}/api/users?=${search}`)
-
-        if(!res.ok) return [new Error(`Error al buscar: ${res.statusText}`)]
+        if (!res.ok) return [new Error(`Error al buscar: ${res.statusText}`)]
         const json = await res.json() as apiSearchResponse
         return [undefined, json.data]
-    } catch (error){
-        if(error instanceof Error) return [error]
+    } catch (error) {
+        if (error instanceof Error) return [error]
     }
 
     return [new Error('Error desconocido')]
